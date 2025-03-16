19aa069693c7d866909b4fdd6b44dcf5683ec534
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-16 12:04:40 +0200

    Add files via upload

---
+++ C# Basics/First Steps in Coding/09. Fish Tank/09. Fish Tank.cs
@@ -0,0 +1,7 @@
+﻿int length = int.Parse(Console.ReadLine());
+int width = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());
+double percent = double.Parse(Console.ReadLine()) / 100;
+double litres = (length * width * height) / 1000.0;
+double result = litres * (1 - percent);
+Console.WriteLine(result);
---
+++ C# Basics/First Steps in Coding/09. Yard Greening/09. Yard Greening.cs
@@ -0,0 +1,5 @@
+﻿double meters = double.Parse(Console.ReadLine());
+double sum = meters * 7.61;
+double discount = sum * 0.18;
+Console.WriteLine($"The final price is: {sum - discount} lv.");
+Console.WriteLine($"The discount is: {discount} lv");
