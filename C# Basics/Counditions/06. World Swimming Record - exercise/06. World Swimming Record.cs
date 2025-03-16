0ad5e33dc73eb8e4792df965c291f1a96ba23d93
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-16 12:11:26 +0200

    Add files via upload

---
+++ C# Basics/Counditions/06. World Swimming Record - exercise/06. World Swimming Record.cs
@@ -0,0 +1,8 @@
+﻿double recordInSeconds = double.Parse(Console.ReadLine());
+double meters =  double.Parse(Console.ReadLine());
+double secondsForOneMeter = double.Parse(Console.ReadLine());
+double hisSends = meters * secondsForOneMeter;
+double plusSeconds = Math.Floor(meters / 15) * 12.5;
+double totalSeconds = plusSeconds + hisSends;
+if (totalSeconds >= recordInSeconds) Console.WriteLine($"No, he failed! He was {totalSeconds-recordInSeconds:f2} seconds slower.");
+else Console.WriteLine($"Yes, he succeeded! The new world record is {totalSeconds:f2} seconds.");
