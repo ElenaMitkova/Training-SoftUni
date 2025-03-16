9c410a409190c14cd5c41ac8c6c9d7ec0edcc808
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-16 12:20:21 +0200

    Add files via upload

---
+++ C# Basics/Advanced Counditions/11. Fruit Shop/11. Fruit Shop.cs
@@ -0,0 +1,87 @@
+﻿string fruit = Console.ReadLine();
+string dayOfTheWeek = Console.ReadLine();
+double quantity = double.Parse(Console.ReadLine());
+double total = 0.0;
+switch (dayOfTheWeek)
+{
+    case "Monday":
+    case "Tuesday":
+    case "Wednesday":
+    case "Thursday":
+    case "Friday":
+        switch (fruit)
+        {
+            case "banana":
+                total = quantity * 2.5;
+                Console.WriteLine($"{total:f2}");
+                break;
+            case "apple":
+                total = quantity * 1.2;
+                Console.WriteLine($"{total:f2}");
+                break;
+            case "orange":
+                total = quantity * 0.85;
+                Console.WriteLine($"{total:f2}");
+                break;
+            case "grapefruit":
+                total = quantity * 1.45;
+                Console.WriteLine($"{total:f2}");
+                break;
+            case "kiwi":
+                total = quantity * 2.7;
+                Console.WriteLine($"{total:f2}");
+                break;
+            case "pineapple":
+                total = quantity * 5.5;
+                Console.WriteLine($"{total:f2}");
+                break;
+            case "grapes":
+                total = quantity * 3.85;
+                Console.WriteLine($"{total:f2}");
+                break;
+            default:
+                Console.WriteLine("error");
+                break;
+        }
+        break;
+    case "Saturday":
+    case "Sunday":
+        switch (fruit)
+        {
+            case "banana":
+                total = quantity * 2.7;
+                Console.WriteLine($"{total:f2}");
+                break;
+            case "apple":
+                total = quantity * 1.25;
+                Console.WriteLine($"{total:f2}");
+                break;
+            case "orange":
+                total = quantity * 0.9;
+                Console.WriteLine($"{total:f2}");
+                break;
+            case "grapefruit":
+                total = quantity * 1.6;
+                Console.WriteLine($"{total:f2}");
+                break;
+            case "kiwi":
+                total = quantity * 3;
+                Console.WriteLine($"{total:f2}");
+                break;
+            case "pineapple":
+                total = quantity * 5.6;
+                Console.WriteLine($"{total:f2}");
+                break;
+            case "grapes":
+                total = quantity * 4.2;
+                Console.WriteLine($"{total:f2}");
+                break;
+            default:
+                Console.WriteLine("error");
+                break;
+        }
+        break;
+    default:
+        Console.WriteLine("error");
+        break;
+}
---
+++ C# Basics/Advanced Counditions/12.Trade/12. Trade Commissions.cs
@@ -0,0 +1,49 @@
+﻿string town = Console.ReadLine();
+double sales = double.Parse(Console.ReadLine());
+double percent = 0.0;
+double total = 0.0;
+switch (town)
+{
+    case "Sofia":
+        if (sales < 0)
+        {
+            Console.WriteLine("error");
+            break;
+        }
+        else if (sales >= 0 && sales <= 500) percent = 5;
+        else if (sales > 500 && sales <= 1000) percent = 7;
+        else if (sales > 1000 && sales <= 10000) percent = 8;
+        else percent = 12;
+        total = sales * percent / 100;
+        Console.WriteLine($"{total:f2}");
+        break;
+    case "Varna":
+        if (sales < 0)
+        {
+            Console.WriteLine("error");
+            break;
+        }
+        else if (sales >= 0 && sales <= 500) percent = 4.5;
+        else if (sales > 500 && sales <= 1000) percent = 7.5;
+        else if (sales > 1000 && sales <= 10000) percent = 10;
+        else percent = 13;
+        total = sales * percent / 100;
+        Console.WriteLine($"{total:f2}");
+        break;
+    case "Plovdiv":
+        if (sales < 0)
+        {
+            Console.WriteLine("error");
+            break;
+        }
+        else if (sales >= 0 && sales <= 500) percent = 5.5;
+        else if (sales > 500 && sales <= 1000) percent = 8;
+        else if (sales > 1000 && sales <= 10000) percent = 12;
+        else percent = 14.5;
+        total = sales * percent / 100;
+        Console.WriteLine($"{total:f2}");
+        break;
+    default:
+        Console.WriteLine("error");
+        break;
+}
