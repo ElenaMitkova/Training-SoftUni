9c1f7a5cac520966d2025711410faa3c43200d0e
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-16 12:15:44 +0200

    Add files via upload

---
+++ C# Basics/Advanced Counditions/02SummerOutfit/02. Summer Outfit.cs
@@ -0,0 +1,66 @@
+﻿int degrees = int.Parse(Console.ReadLine());
+string partOfTheDay = Console.ReadLine();
+string outfit, shoes;
+if (degrees >= 10 && degrees <= 18)
+{
+	switch (partOfTheDay)
+	{
+		case "Morning":
+			outfit = "Sweatshirt";
+			shoes = "Sneakers";
+            Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
+			break;
+		case "Afternoon":
+            outfit = "Shirt";
+            shoes = "Moccasins";
+            Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
+            break;
+        case "Evening":
+            outfit = "Shirt";
+            shoes = "Moccasins";
+            Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
+            break;
+    }
+}
+else if (degrees > 18 && degrees <= 24)
+{
+    switch (partOfTheDay)
+    {
+        case "Morning":
+            outfit = "Shirt";
+            shoes = "Moccasins";
+            Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
+            break;
+        case "Afternoon":
+            outfit = "T-Shirt";
+            shoes = "Sandals";
+            Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
+            break;
+        case "Evening":
+            outfit = "Shirt";
+            shoes = "Moccasins";
+            Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
+            break;
+    }
+}
+else
+{
+    switch (partOfTheDay)
+    {
+        case "Morning":
+            outfit = "T-Shirt";
+            shoes = "Sandals";
+            Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
+            break;
+        case "Afternoon":
+            outfit = "Swim Suit";
+            shoes = "Barefoot";
+            Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
+            break;
+        case "Evening":
+            outfit = "Shirt";
+            shoes = "Moccasins";
+            Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
+            break;
+    }
+}
---
+++ C# Basics/Advanced Counditions/03. New House/03. New House.cs
@@ -0,0 +1,44 @@
+﻿string flower = Console.ReadLine();
+int flowerCount = int.Parse(Console.ReadLine());
+int budget = int.Parse(Console.ReadLine());
+double total = 0;
+switch (flower)
+{
+	case "Roses":
+		total = flowerCount * 5;
+        if (flowerCount > 80)
+        {
+            total = total - (total * 0.1);
+        }
+		break;
+	case "Dahlias":
+        total = flowerCount * 3.8;
+        if (flowerCount > 90)
+        {
+            total = total - (total * 0.15);
+        }
+        break;
+    case "Tulips":
+        total = flowerCount * 2.8;
+        if (flowerCount > 80)
+        {
+            total = total - (total * 0.15);
+        }
+        break;
+    case "Narcissus":
+        total = flowerCount * 3;
+        if (flowerCount < 120)
+        {
+            total = total + (total * 0.15);
+        }
+        break;
+    case "Gladiolus":
+        total = flowerCount * 2.5;
+        if (flowerCount < 80)
+        {
+            total = total + (total * 0.2);
+        }
+        break;
+}
+if (budget >= total) Console.WriteLine($"Hey, you have a great garden with {flowerCount} {flower} and {budget - total:f2} leva left.");
+else Console.WriteLine($"Not enough money, you need {total - budget:f2} leva more.");
