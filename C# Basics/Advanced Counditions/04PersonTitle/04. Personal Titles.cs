4aa0d3dad7f44116ecd48e0224b0b24bcb155731
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-16 12:17:18 +0200

    Add files via upload

---
+++ C# Basics/Advanced Counditions/04PersonTitle/04. Personal Titles.cs
@@ -0,0 +1,13 @@
+﻿double age = double.Parse(Console.ReadLine());
+char femaleOrMale = char.Parse(Console.ReadLine());
+switch (femaleOrMale)
+{
+	case 'm':
+		if (age >= 16) Console.WriteLine("Mr.");
+		else Console.WriteLine("Master");
+		break;
+	case 'f':
+		if (age >= 16) Console.WriteLine("Ms.");
+		else Console.WriteLine("Miss");
+		break;
+}
---
+++ C# Basics/Advanced Counditions/05. Small Shop/05. Small Shop.cs
@@ -0,0 +1,76 @@
+﻿string beverage = Console.ReadLine();
+string city = Console.ReadLine();
+double packs = double.Parse(Console.ReadLine());
+switch (beverage)
+{
+	case "coffee":
+		switch (city)
+		{
+			case "Sofia":
+                Console.WriteLine(packs * 0.5);
+                break;
+			case "Plovdiv":
+                Console.WriteLine(packs * 0.4);
+                break;
+			case "Varna":
+                Console.WriteLine(packs * 0.45);
+                break;
+		}
+        break;
+	case "water":
+        switch (city)
+        {
+            case "Sofia":
+                Console.WriteLine(packs * 0.8);
+                break;
+            case "Plovdiv":
+                Console.WriteLine(packs * 0.7);
+                break;
+            case "Varna":
+                Console.WriteLine(packs * 0.7);
+                break;
+        }
+        break;
+    case "beer":
+        switch (city)
+        {
+            case "Sofia":
+                Console.WriteLine(packs * 1.2);
+                break;
+            case "Plovdiv":
+                Console.WriteLine(packs * 1.15);
+                break;
+            case "Varna":
+                Console.WriteLine(packs * 1.1);
+                break;
+        }
+        break;
+    case "sweets":
+        switch (city)
+        {
+            case "Sofia":
+                Console.WriteLine(packs * 1.45);
+                break;
+            case "Plovdiv":
+                Console.WriteLine(packs * 1.3);
+                break;
+            case "Varna":
+                Console.WriteLine(packs * 1.35);
+                break;
+        }
+        break;
+    case "peanuts":
+        switch (city)
+        {
+            case "Sofia":
+                Console.WriteLine(packs * 1.6);
+                break;
+            case "Plovdiv":
+                Console.WriteLine(packs * 1.5);
+                break;
+            case "Varna":
+                Console.WriteLine(packs * 1.55);
+                break;
+        }
+        break;
+}
