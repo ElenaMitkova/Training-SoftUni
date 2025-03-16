7a919232c93229ad379b9030843b8043cae11882
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-16 12:18:27 +0200

    Add files via upload

---
+++ C# Basics/Advanced Counditions/07WorkingHours/07WorkingHours.cs
@@ -0,0 +1,8 @@
+﻿int hour = int.Parse(Console.ReadLine());
+string dayOfTheWeek  = Console.ReadLine();
+if (hour >= 10 && hour <= 18)
+{
+    if (dayOfTheWeek != "Sunday") Console.WriteLine("open");
+    else Console.WriteLine("closed");
+}
+else Console.WriteLine("closed");
