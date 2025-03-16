a8b00da240c6add30e26a92dae0467c72968449a
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-16 12:07:21 +0200

    Add files via upload

---
+++ C# Basics/Counditions/03. Time + 15 Minutes/03. Time + 15 Minutes.cs
@@ -0,0 +1,11 @@
+﻿int hours = int.Parse(Console.ReadLine());
+int minutes = int.Parse(Console.ReadLine());
+minutes += 15;
+if (minutes > 59)
+{
+    hours += 1;
+    minutes -= 60;
+}
+if (hours > 23) hours -= 24;
+if (minutes < 10) Console.WriteLine($"{hours}:0{minutes}");
+else Console.WriteLine($"{hours}:{minutes}");
