15a693d14d545a75eca8bc428a2f6bc7a777a6b8
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-17 23:31:00 +0200

    Add files via upload

---
+++ C# Fundamentals/Methods/11.Mathoperations/11.Mathoperations.cs
@@ -0,0 +1,23 @@
+﻿int firstNumber = int.Parse(Console.ReadLine());
+char operation = char.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
+Console.WriteLine(GetResult(firstNumber, operation, secondNumber));
+static int GetResult(int firstNumber, char operation, int secondNumber)
+{
+    if (operation == '+')
+    {
+        return firstNumber + secondNumber;
+    }
+    else if(operation == '-')
+    {
+        return Math.Abs(secondNumber - firstNumber);
+    }
+    else if(operation == '*')
+    {
+        return firstNumber * secondNumber;
+    }
+    else
+    {
+        return firstNumber / secondNumber;
+    }
+}
