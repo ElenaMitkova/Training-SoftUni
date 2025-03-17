ac2bd1c77fc83a3baa231d0d6c9e1a061652c3a7
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-17 20:50:10 +0200

    Add files via upload

---
+++ C# Fundamentals/Basic Syntax/06.Strongnumber/06.Strongnumber-exercise.cs
@@ -0,0 +1,16 @@
+﻿int number = int.Parse(Console.ReadLine());
+int numberCopy = number;
+int sum = 0;
+while (numberCopy > 0)
+{
+    int factorial = numberCopy % 10;
+    int sumFac = 1;
+    for (int i = 1; i <= factorial; i++)
+    {
+        sumFac *= i;
+    }
+    sum += sumFac;
+    numberCopy /= 10;
+}
+if (sum == number) Console.WriteLine("yes");
+else Console.WriteLine("no");
