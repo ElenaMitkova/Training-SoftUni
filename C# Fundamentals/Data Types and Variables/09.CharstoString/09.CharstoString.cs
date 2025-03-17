53b12c35e97cabbb92a6e72b7f331a23560f7af4
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-17 23:01:01 +0200

    Add files via upload

---
+++ C# Fundamentals/Data Types and Variables/09.CharstoString/09.CharstoString.cs
@@ -0,0 +1,7 @@
+﻿string output = "";
+for (int i = 0; i < 3; i++)
+{
+    char text = char.Parse(Console.ReadLine());
+    output += text;
+}
+Console.WriteLine(output);
---
+++ C# Fundamentals/Data Types and Variables/09.SpiceMustFlow-exercise/09.SpiceMustFlow-exercise.cs
@@ -0,0 +1,24 @@
+﻿int yelts = int.Parse(Console.ReadLine());
+int drops = 10;
+int consumers = 26;
+int counter = 0;
+int yeltsCopy = yelts;
+int sum = 0;
+while (yelts >= 100)
+{
+    counter++;
+    yeltsCopy -= consumers;
+    sum += yeltsCopy;
+    yelts -= drops;
+    yeltsCopy = yelts;
+}
+if (sum < 26)
+{
+    Console.WriteLine(counter);
+    Console.WriteLine(sum);
+}
+else
+{
+    Console.WriteLine(counter);
+    Console.WriteLine(sum - consumers);
+}
