ed82666cf6b2474b91f89acc83fad5ad07bfb927
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-17 23:03:31 +0200

    Add files via upload

---
+++ C# Fundamentals/Data Types and Variables/12.RefactorSpecialNumbers/12.RefactorSpecialNumbers.cs
@@ -0,0 +1,14 @@
+﻿int countNumber = int.Parse(Console.ReadLine());
+bool isEqual = false;
+for (int i = 1; i <= countNumber; i++)
+{
+    int sum = 0;
+    int number = i;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number = number / 10;
+    }
+    isEqual = (sum == 5) || (sum == 7) || (sum == 11);
+    Console.WriteLine("{0} -> {1}", i, isEqual);
+}
