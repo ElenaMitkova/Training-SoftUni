778a0f36f49b37adec2f27c3d29731d02d5c42e4
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-17 20:49:07 +0200

    Add files via upload

---
+++ C# Fundamentals/Basic Syntax/05.Login/05.Login-exercise.cs
@@ -0,0 +1,25 @@
+﻿string username = Console.ReadLine();
+string password = "";
+for (int i = username.Length - 1; i >= 0; i--)
+{
+    password += username[i];
+}
+int attempts = 4;
+while (attempts > 0)
+{
+    attempts--;
+    if (Console.ReadLine() == password)
+    {
+        Console.WriteLine($"User {username} logged in.");
+        return;
+    }
+    else
+    {
+        if (attempts == 0)
+        {
+            Console.WriteLine($"User {username} blocked!");
+            return;
+        }
+        else Console.WriteLine("Incorrect password. Try again.");
+    }
+}
---
+++ C# Fundamentals/Basic Syntax/05.Messages-MoreExercises/05.Messages-MoreExercises.cs
@@ -0,0 +1,51 @@
+﻿int countOfTheWord = int.Parse(Console.ReadLine());
+string output = "";
+while (countOfTheWord > 0)
+{
+    string input = Console.ReadLine();
+    if (input.Length == 1)
+    {
+        int number = int.Parse(input[0].ToString());
+        if (number == 0) output += " ";
+        else if (number == 2) output += "a";
+        else if(number == 3) output += "d";
+        else if(number == 4) output += "g";
+        else if(number == 5) output += "j";
+        else if(number == 6) output += "m";
+        else if(number == 7) output += "p";
+        else if(number == 8) output += "t";
+        else if(number == 9) output += "w";
+    }
+    else if (input.Length == 2)
+    {
+        int number = int.Parse(input[0].ToString());
+        if (number == 2) output += "b";
+        else if(number == 3) output += "e";
+        else if(number == 4) output += "h";
+        else if(number == 5) output += "k";
+        else if(number == 6) output += "n";
+        else if(number == 7) output += "q";
+        else if(number == 8) output += "u";
+        else if(number == 9) output += "x";
+    }
+    else if (input.Length == 3) 
+    {
+        int number = int.Parse(input[0].ToString());
+        if (number == 2) output += "c";
+        else if (number == 3) output += "f";
+        else if (number == 4) output += "i";
+        else if (number == 5) output += "l";
+        else if (number == 6) output += "o";
+        else if (number == 7) output += "r";
+        else if (number == 8) output += "v";
+        else if (number == 9) output += "y";
+    }
+    else
+    {
+        int number = int.Parse(input[0].ToString());
+        if (number == 7) output += "s";
+        else if (number == 9) output += "z";
+    }
+    countOfTheWord--;
+}
+Console.WriteLine(output);
