7c645be7bf83f9c750de1622ac7ec94c856d41f8
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-17 23:12:02 +0200

    Add files via upload

---
+++ C# Fundamentals/Arrays/04.FoldandSum-MoreExercise/04.FoldandSum-MoreExercise.cs
@@ -0,0 +1,25 @@
+﻿int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+int k = numbers.Length / 2;
+int[]first = new int[k];
+int [] second = new int[k];
+int countFirst = 0;
+int countSecond = 0;
+for (int i = k/2 - 1; i >= 0; i--)
+{
+    first[countFirst] = numbers[i];
+    countFirst++;
+}
+for (int i = numbers.Length - 1;i >= numbers.Length - k / 2; i--)
+{
+    first[countFirst] = numbers[i];
+    countFirst++;
+}
+for (int i = k - k/2;i < numbers.Length - k/2; i++)
+{
+    second[countSecond] = numbers[i];
+    countSecond++;
+}
+for (int i = 0; i < first.Length; i++)
+{
+    Console.Write($"{first[i] + second[i]} ");
+}
---
+++ C# Fundamentals/Arrays/04.ReverseArrayofStrings/04.ReverseArrayofStrings.cs
@@ -0,0 +1,8 @@
+﻿string[] stringsArray = Console.ReadLine().Split();
+for (int i = 0; i < stringsArray.Length / 2; i++)
+{
+    string temp = stringsArray[i];
+    stringsArray[i] = stringsArray[stringsArray.Length - 1 - i];
+    stringsArray[stringsArray.Length - 1 - i] = temp;
+}
+Console.WriteLine(string.Join(" ",stringsArray));
