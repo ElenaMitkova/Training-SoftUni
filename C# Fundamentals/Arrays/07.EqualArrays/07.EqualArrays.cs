e76a9d9030aa1768d9e99bd445c9f66d9fc3e478
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-17 23:15:10 +0200

    Add files via upload

---
+++ C# Fundamentals/Arrays/07.EqualArrays/07.EqualArrays.cs
@@ -0,0 +1,29 @@
+﻿int[] numbersLine = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int[] numbersSecondLine = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int sum = 0;
+if (numbersLine.Length < numbersSecondLine.Length)
+{
+    for (int i = 0; i < numbersLine.Length; i++)
+    {
+        if (numbersLine[i] == numbersSecondLine[i]) sum += numbersLine[i];
+        else
+        {
+            Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+            return;
+        }
+    }
+    Console.WriteLine($"Arrays are identical. Sum: {sum}");
+}
+else
+{
+    for (int i = 0; i < numbersSecondLine.Length; i++)
+    {
+        if (numbersSecondLine[i] == numbersLine[i]) sum += numbersSecondLine[i];
+        else
+        {
+            Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+            return;
+        }
+    }
+    Console.WriteLine($"Arrays are identical. Sum: {sum}");
+}
---
+++ C# Fundamentals/Arrays/07.MaxSequenceofEqualElements-exercise/07.MaxSequenceofEqualElements-exercise.cs
@@ -0,0 +1,28 @@
+﻿int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+int max = 1;
+int integer = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    int count = 1;
+    for (int j = i + 1; j < numbers.Length; j++)
+    {
+        if (numbers[j] == numbers[i])
+        {
+            count++;
+        }
+        else
+        {
+            break;
+        }
+    }
+    if (count > max)
+    {
+        max = count;
+        integer = numbers[i];
+    }
+}
+for (int i = 0; i < max - 1; i++)
+{
+    Console.Write($"{integer} ");
+}
+Console.WriteLine(integer);
