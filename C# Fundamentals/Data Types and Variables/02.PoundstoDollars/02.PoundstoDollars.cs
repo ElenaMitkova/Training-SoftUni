c1f3fc1a2b0016ff90f7954f6b9cfe29fb794662
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-17 22:53:26 +0200

    Add files via upload

---
+++ C# Fundamentals/Data Types and Variables/02.PoundstoDollars/02.PoundstoDollars.cs
@@ -0,0 +1,4 @@
+﻿decimal pounds = decimal.Parse(Console.ReadLine());
+double poundsToDollars = 1.31;
+decimal dollars = pounds * (decimal)poundsToDollars;
+Console.WriteLine($"{dollars:F3}");
---
+++ C# Fundamentals/Data Types and Variables/02.SumDigits-exercise/02.SumDigits-exercise.cs
@@ -0,0 +1,9 @@
+﻿int number = int.Parse(Console.ReadLine());
+byte sum = 0;
+while (number > 0)
+{
+    int divide = number % 10;
+    sum += (byte)divide;
+    number /= 10;
+}
+Console.WriteLine(sum);
