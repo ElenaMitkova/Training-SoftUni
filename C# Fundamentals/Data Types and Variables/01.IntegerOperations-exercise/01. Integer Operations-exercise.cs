dcd51db0761255cca23bd2d21187ea8e694914f8
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-17 22:51:50 +0200

    Add files via upload

---
+++ C# Fundamentals/Data Types and Variables/01.IntegerOperations-exercise/01. Integer Operations-exercise.cs
@@ -0,0 +1,5 @@
+﻿int firstNumber = int.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
+int thirdNumber = int.Parse(Console.ReadLine());
+int fourthNumber = int.Parse(Console.ReadLine());
+Console.WriteLine((firstNumber + secondNumber) / thirdNumber * fourthNumber);
