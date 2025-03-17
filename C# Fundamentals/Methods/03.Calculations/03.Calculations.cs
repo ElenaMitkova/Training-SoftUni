47a981cc1217f13e8520beecc7ef56a8b18ce435
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-17 23:22:23 +0200

    Add files via upload

---
+++ C# Fundamentals/Methods/03.Calculations/03.Calculations.cs
@@ -0,0 +1,35 @@
+﻿string command = Console.ReadLine();
+int firstNumber = int.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
+if (command == "add")
+{
+    Add(firstNumber, secondNumber);
+}
+else if (command == "multiply")
+{
+    Multiply(firstNumber, secondNumber);
+}
+else if(command == "subtract")
+{
+    Subtract(firstNumber, secondNumber);
+}
+else
+{
+    Divide(firstNumber, secondNumber);
+}
+static void Add(int firstNumber, int secondNumber)
+{
+    Console.WriteLine(firstNumber + secondNumber);
+}
+static void Multiply(int firstNumber, int secondNumber)
+{
+    Console.WriteLine(firstNumber * secondNumber);
+}
+static void Subtract(int firstNumber, int secondNumber)
+{
+    Console.WriteLine(Math.Abs(firstNumber-secondNumber));
+}
+static void Divide(int firstNumber, int secondNumber)
+{
+    Console.WriteLine(firstNumber / secondNumber);
+}
---
+++ C# Fundamentals/Methods/03.CharactersinRange-Exercise/03.CharactersinRange-Exercise.cs
@@ -0,0 +1,21 @@
+﻿char startChar = char.Parse(Console.ReadLine());
+char endChar = char.Parse(Console.ReadLine());
+Characters(startChar, endChar);
+static void Characters(char startChar, char endChar)
+{
+    string output = "";
+    if (startChar > endChar)
+    {
+        for (int i = endChar + 1; i < startChar; i++)
+        {
+            output += (char)i + " ";
+        }
+        Console.WriteLine(output.Trim(' '));
+        return;
+    }
+    for (int i = startChar + 1; i < endChar; i++)
+    {
+        output += (char)i + " ";
+    }
+    Console.WriteLine(output.Trim(' '));
+}
