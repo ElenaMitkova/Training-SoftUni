ddc63487a204895ae223e139d0d01025f36495fa
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-17 23:28:38 +0200

    Add files via upload

---
+++ C# Fundamentals/Methods/09.GreaterofTwoValues/09.GreaterofTwoValues.cs
@@ -0,0 +1,53 @@
+﻿string type = Console.ReadLine();
+if (type == "int")
+{
+    int firstNumber = int.Parse(Console.ReadLine());
+    int secondNumber = int.Parse(Console.ReadLine());
+    Console.WriteLine(GetMaxInt(firstNumber, secondNumber));
+}
+else if (type == "char")
+{
+    char firstChar = char.Parse(Console.ReadLine());
+    char secondChar = char.Parse(Console.ReadLine());
+    Console.WriteLine(GetMaxChar(firstChar, secondChar));
+}
+else
+{
+    string firstString = Console.ReadLine();
+    string secondString = Console.ReadLine();
+    Console.WriteLine(GetMaxString(firstString, secondString));
+}
+static int GetMaxInt(int firstNumber, int secondNumber)
+{
+    if (firstNumber > secondNumber)
+    {
+        return firstNumber;
+    }
+    else
+    {
+        return secondNumber;
+    }
+}
+static char GetMaxChar(char firstChar, char secondChar)
+{
+    if (firstChar > secondChar)
+    {
+        return firstChar;
+    }
+    else
+    {
+        return secondChar;
+    }
+}
+static string GetMaxString(string firstString, string secondString)
+{
+    int output = firstString.CompareTo(secondString);
+    if (output == 1)
+    {
+        return firstString;
+    }
+    else
+    {
+        return secondString;
+    }
+}
---
+++ C# Fundamentals/Methods/09.PalindromeIntegers-Exercise/09.PalindromeIntegers-Exercise.cs
@@ -0,0 +1,25 @@
+﻿string input = Console.ReadLine();
+while (input != "END")
+{
+    int number = int.Parse(input);
+    if (IsPalindrome(number))
+    {
+        Console.WriteLine("true");
+    }
+    else
+    {
+        Console.WriteLine("false");
+    }
+    input = Console.ReadLine();
+}
+static bool IsPalindrome(int number)
+{
+    string palindrome = "";
+    int numberCopy = number;
+    while (number > 0)
+    {
+        palindrome += number % 10;
+        number /= 10;
+    }
+    return int.Parse(palindrome) == numberCopy;
+}
