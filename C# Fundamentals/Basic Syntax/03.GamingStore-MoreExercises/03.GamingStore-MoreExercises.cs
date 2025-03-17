4b6254a36c03ec5e7da0f5975e0d369791d1b7ab
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-17 20:46:29 +0200

    Add files via upload

---
+++ C# Fundamentals/Basic Syntax/03.GamingStore-MoreExercises/03.GamingStore-MoreExercises.cs
@@ -0,0 +1,102 @@
+﻿double balance = double.Parse(Console.ReadLine());
+double balanceCopy = balance;
+string game = Console.ReadLine();
+while (game != "Game Time")
+{
+    if (balance == 0)
+    {
+        Console.WriteLine("Out of money!");
+        return;
+    }
+    switch (game)
+	{
+		case "OutFall 4":
+            if (balance == 0)
+            {
+                Console.WriteLine("Out of money!");
+                return;
+            }
+            else if (balance >= 39.99)
+            {
+                Console.WriteLine($"Bought {game}");
+                balance -= 39.99;
+            }
+            else Console.WriteLine("Too Expensive");
+			break;
+		case "CS: OG":
+            if (balance == 0)
+            {
+                Console.WriteLine("Out of money!");
+                return;
+            }
+            else if (balance >= 15.99)
+            {
+                Console.WriteLine($"Bought {game}");
+                balance -= 15.99;
+            }
+            else Console.WriteLine("Too Expensive");
+            break;
+		case "Zplinter Zell":
+            if (balance == 0)
+            {
+                Console.WriteLine("Out of money!");
+                return;
+            }
+            else if (balance >= 19.99)
+            {
+                Console.WriteLine($"Bought {game}");
+                balance -= 19.99;
+            }
+            else Console.WriteLine("Too Expensive");
+            break;
+        case "Honored 2":
+            if (balance == 0)
+            {
+                Console.WriteLine("Out of money!");
+                return;
+            }
+            else if (balance >= 59.99)
+            {
+                Console.WriteLine($"Bought {game}");
+                balance -= 59.99;
+            }
+            else Console.WriteLine("Too Expensive");
+            break;
+        case "RoverWatch":
+            if (balance == 0)
+            {
+                Console.WriteLine("Out of money!");
+                return;
+            }
+            else if (balance >= 29.99)
+            {
+                Console.WriteLine($"Bought {game}");
+                balance -= 29.99;
+            }
+            else Console.WriteLine("Too Expensive");
+            break;
+        case "RoverWatch Origins Edition":
+            if (balance == 0)
+            {
+                Console.WriteLine("Out of money!");
+                return;
+            }
+            else if (balance >= 39.99)
+            {
+                Console.WriteLine($"Bought {game}");
+                balance -= 39.99;
+            }
+            else Console.WriteLine("Too Expensive");
+            break;
+        default:
+            Console.WriteLine("Not Found");
+			break;
+	}
+    game = Console.ReadLine();
+}
+if (balance == 0)
+{
+    Console.WriteLine("Out of money!");
+    return;
+}
+Console.WriteLine($"Total spent: ${balanceCopy - balance:F2}. Remaining: ${balance:F2}");
---
+++ C# Fundamentals/Basic Syntax/03.Vacation/03.Vacation-exercise.cs
@@ -0,0 +1,26 @@
+﻿int countPeople = int.Parse(Console.ReadLine());
+string typePeople = Console.ReadLine();
+string dayOfTheWeek = Console.ReadLine();
+double total = 0.0;
+switch (typePeople)
+{
+	case "Students":
+		if (dayOfTheWeek == "Friday") total = countPeople * 8.45;
+		else if (dayOfTheWeek == "Saturday") total = countPeople * 9.8;
+		else total = countPeople * 10.46;
+        if (countPeople >= 30) total -= total * 0.15;
+		break;
+	case "Business":
+		if (dayOfTheWeek == "Friday") total = countPeople * 10.9;
+        else if (dayOfTheWeek == "Saturday") total = countPeople * 15.6;
+        else total = countPeople * 16;
+        if (countPeople >= 100) total -= total/10;
+        break;
+    case "Regular":
+        if (dayOfTheWeek == "Friday") total = countPeople * 15;
+        else if (dayOfTheWeek == "Saturday") total = countPeople * 20;
+        else total = countPeople * 22.5;
+        if (countPeople >= 10 && countPeople <= 20) total -= total * 0.05;
+        break;
+}
+Console.WriteLine($"Total price: {total:F2}");
