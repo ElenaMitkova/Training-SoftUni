5ca9500f81835b1a1a49a3785b488f2f9c18e86b
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-17 20:47:39 +0200

    Add files via upload

---
+++ C# Fundamentals/Basic Syntax/04.BackIn30Minutes/04.BackIn30Minutes.cs
@@ -0,0 +1,11 @@
+﻿int hour = int.Parse(Console.ReadLine());
+int minutes = int.Parse(Console.ReadLine());
+minutes += 30;
+if (minutes > 59)
+{
+    minutes -= 60;
+    hour++;
+}
+if (hour > 23) hour -= 24;
+if (minutes < 10) Console.WriteLine($"{hour}:0{minutes}");
+else Console.WriteLine($"{hour}:{minutes}");
