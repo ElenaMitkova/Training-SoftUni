35e11d9a6939c947540aecb1b5514b33c700844a
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-17 22:57:49 +0200

    Add files via upload

---
+++ C# Fundamentals/Data Types and Variables/06.BalancBracket-MoreExercise/06.BalancBracket-MoreExercise.cs
@@ -0,0 +1,33 @@
+﻿int n = int.Parse(Console.ReadLine());
+int openCount = 0;
+bool lastWasOpen = false;
+for (int i = 0; i < n; i++)
+{
+    string line = Console.ReadLine();
+    foreach (char ch in line)
+    {
+        if (ch == '(')
+        {
+            if (lastWasOpen)
+            {
+                Console.WriteLine("UNBALANCED");
+                return;
+            }
+            openCount++;
+            lastWasOpen = true;
+        }
+        else if (ch == ')')
+        {
+            if (openCount == 0)
+            {
+                Console.WriteLine("UNBALANCED");
+                return;
+            }
+            openCount--;
+            lastWasOpen = false;
+        }
+        else lastWasOpen = false;
+    }
+}
+if (openCount == 0) Console.WriteLine("BALANCED");
+else Console.WriteLine("UNBALANCED");
---
+++ C# Fundamentals/Data Types and Variables/06.TriplesLatinLetter-exercise/06.TriplesLatinLetter-exercise.cs
@@ -0,0 +1,12 @@
+﻿byte counter = byte.Parse(Console.ReadLine());
+int number = 'a' + counter;
+for (char i = 'a'; i < number; i++)
+{
+    for (char j = 'a'; j < number; j++)
+    {
+        for (char k = 'a'; k < number; k++)
+        {
+            Console.WriteLine($"{i}{j}{k}");
+        }
+    }
+}
