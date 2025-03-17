5abf6328e7b5651a0e9ca7fe19b4fa0779b286bb
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-17 23:27:43 +0200

    Add files via upload

---
+++ C# Fundamentals/Methods/08.FactorialDivision-Exercise/08.FactorialDivision-Exercise.cs
@@ -0,0 +1,12 @@
+﻿long firstNumber = long.Parse(Console.ReadLine());
+long secondNumber = long.Parse(Console.ReadLine());
+Console.WriteLine($"{Factorial(firstNumber)/ Factorial(secondNumber):F2}");
+static double Factorial(long number)
+{
+    double result = 1;
+    for (long i = 1; i <= number; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
---
+++ C# Fundamentals/Methods/08.MathPower/08.MathPower.cs
@@ -0,0 +1,12 @@
+﻿double baseNumber = double.Parse(Console.ReadLine());
+int power = int.Parse(Console.ReadLine());
+Console.WriteLine(GetPower(baseNumber,power));
+static double GetPower(double baseNumber, int power)
+{
+    double output = 1;
+    for (int i = 0; i < power; i++)
+    {
+        output *= baseNumber;
+    }
+    return output;
+}
