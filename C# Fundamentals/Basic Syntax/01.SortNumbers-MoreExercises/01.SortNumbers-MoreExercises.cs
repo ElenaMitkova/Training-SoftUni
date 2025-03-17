f1aa7d69e7c9c8024e8a232eb88bbcf8113cc0e1
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-17 20:44:02 +0200

    Add files via upload

---
+++ C# Fundamentals/Basic Syntax/01.SortNumbers-MoreExercises/01.SortNumbers-MoreExercises.cs
@@ -0,0 +1,24 @@
+﻿int firstNumber = int.Parse(Console.ReadLine());
+int secondfNumber = int.Parse(Console.ReadLine());
+int thirdNumber = int.Parse(Console.ReadLine());
+int[] array = new int[3];
+array[0] = firstNumber;
+array[1] = secondfNumber;
+array[2] = thirdNumber;
+int temp = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    for (int j = 0; j < array.Length - 1; j++)
+    {
+        if (array[j] < array[j + 1])
+        {
+            temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+        }
+    }
+}
+for (int i = 0;i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
