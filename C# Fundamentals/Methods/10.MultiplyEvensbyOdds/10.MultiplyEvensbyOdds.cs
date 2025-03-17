caad5110293688f20072c763288f27a5fea4107e
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-17 23:29:47 +0200

    Add files via upload

---
+++ C# Fundamentals/Methods/10.MultiplyEvensbyOdds/10.MultiplyEvensbyOdds.cs
@@ -0,0 +1,32 @@
+﻿int number = Math.Abs(int.Parse(Console.ReadLine()));
+Console.WriteLine(GetMultipleOfEvenAndOdds(number));
+static int GetSumOfEvenDigits(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        if(number % 10 % 2 == 0)
+        {
+            sum += number % 10;
+        }
+        number /= 10;
+    }
+    return sum;
+}
+static int GetSumOfOddDigits(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        if (number % 10 % 2 == 1)
+        {
+            sum += number % 10;
+        }
+        number /= 10;
+    }
+    return sum;
+}
+static int GetMultipleOfEvenAndOdds(int number)
+{ 
+    return GetSumOfEvenDigits(number) * GetSumOfOddDigits(number);
+}
---
+++ C# Fundamentals/Methods/10.TopNumber-Exercise/10.TopNumber-Exercise.cs
@@ -0,0 +1,38 @@
+﻿int counter = int.Parse(Console.ReadLine());
+TopNumbers(counter);
+static void TopNumbers(int counter)
+{
+    for (int i = 1; i <= counter; i++)
+    {
+        if(DivisibleBy8(i) && OneOddDigit(i))
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+static bool DivisibleBy8(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    if (sum % 8 == 0)
+    {
+        return true;
+    }
+    return false;
+}
+static bool OneOddDigit(int number)
+{
+    while (number > 0)
+    {
+        if (number % 10 % 2 == 1)
+        {
+            return true;
+        }
+        number /= 10;
+    }
+    return false;
+}
