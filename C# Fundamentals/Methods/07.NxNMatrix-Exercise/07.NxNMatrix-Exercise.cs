860ad789ed66b142df8859ce3dc9084566c65625
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-17 23:26:48 +0200

    Add files via upload

---
+++ C# Fundamentals/Methods/07.NxNMatrix-Exercise/07.NxNMatrix-Exercise.cs
@@ -0,0 +1,14 @@
+﻿int number = int.Parse(Console.ReadLine());
+PrintMatrix(number);
+static void PrintMatrix(int number)
+{
+    for (int i = 0; i < number; i++)
+    {
+        Console.Write(number);
+        for (int j = 0; j < number - 1; j++)
+        {
+            Console.Write(" " + number);
+        }
+        Console.WriteLine();
+    }
+}
---
+++ C# Fundamentals/Methods/07.RepeatString/07.RepeatString.cs
@@ -0,0 +1,12 @@
+﻿string input = Console.ReadLine();
+int counter = int.Parse(Console.ReadLine());
+Console.WriteLine(GetString(input, counter));
+static string GetString(string input, int counter)
+{
+    string output = "";
+    for (int i = 0; i < counter; i++)
+    {
+        output += input;
+    }
+    return output;
+}
