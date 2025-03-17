0af4dc8f8f0be5a38d48f5c0898d7a22394be8d7
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-17 23:25:39 +0200

    Add files via upload

---
+++ C# Fundamentals/Methods/06.CalculateRectangleArea/06.CalculateRectangleArea.cs
@@ -0,0 +1,7 @@
+﻿double width = double.Parse(Console.ReadLine());
+double height = double.Parse(Console.ReadLine());
+Console.WriteLine(GetArea(width,height));
+static double GetArea(double width, double height)
+{
+    return width * height;
+}
---
+++ C# Fundamentals/Methods/06.MiddleCharacters-Exercise/06.MiddleCharacters-Exercise.cs
@@ -0,0 +1,20 @@
+﻿string text = Console.ReadLine();
+if (text.Length % 2 == 0)
+{
+    Console.WriteLine(ReturnTwoStings(text));
+    return;
+}
+Console.WriteLine(ReturnOneSting(text));
+static string ReturnTwoStings(string text)
+{
+    string output = "";
+    for (int i = text.Length / 2 - 1; i <= text.Length / 2; i++)
+    {
+        output += text[i];
+    }
+    return output;
+}
+static char ReturnOneSting(string text)
+{
+    return text[text.Length / 2];
+}
