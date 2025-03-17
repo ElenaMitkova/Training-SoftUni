3e524045799eb3948de899aead3763bcb819ea97
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-17 23:17:40 +0200

    Add files via upload

---
+++ C# Fundamentals/Arrays/09.KaminoFactory-exercise/09.KaminoFactory-exercise.cs
@@ -0,0 +1,43 @@
+﻿int stringsLength = int.Parse(Console.ReadLine());
+string command;
+int max = 0;
+int index = int.MaxValue;
+int maxSum = 0;
+int sample = 1;
+int bestSampleNumber = 0;
+int[] bestDna = new int[stringsLength];
+while ((command = Console.ReadLine()) != "Clone them!")
+{
+    int sum = 0;
+    int[] dna = command.Split('!', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+    for (int i = 0; i < dna.Length; i++)
+    {
+        sum += dna[i];
+    }
+    for (int i = 0; i < dna.Length; i++)
+    {
+        int count = 1;
+        for (int j = i + 1; j < dna.Length; j++)
+        {
+            if (dna[i] == 1 && dna[i] == dna[j])
+            {
+                count++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        if (count > max || (count == max && i < index) || (count == max && i == index && sum > maxSum))
+        {
+            max = count;
+            maxSum = sum;
+            index = i;
+            bestDna = dna;
+            bestSampleNumber = sample;
+        }
+    }
+    sample++;
+}
+Console.WriteLine($"Best DNA sample {bestSampleNumber} with sum: {maxSum}.");
+Console.WriteLine(string.Join(" ", bestDna));
---
+++ C# Fundamentals/Arrays/10.LadyBugs-exercise/10.LadyBugs-exercise.cs
@@ -0,0 +1,71 @@
+﻿long filedSize = long.Parse(Console.ReadLine());
+long[] filed = new long[filedSize];
+int[] ladyBugs = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+for (int i = 0; i < ladyBugs.Length; i++)
+{
+	if (ladyBugs[i] >= 0 && ladyBugs[i] < filed.Length)
+	{
+        int p = ladyBugs[i];
+		filed[p] = 1;
+	}
+}
+string command;
+while ((command = Console.ReadLine())!="end")
+{
+	string[] bugsCommands = command.Split(' ');
+	int indexBug = int.Parse(bugsCommands[0]);
+	string direction = bugsCommands[1];
+	int steps = int.Parse(bugsCommands[2]);
+	if (indexBug<0||indexBug>=filed.Length)
+	{
+		continue;
+	}
+	if (filed[indexBug] == 1)
+	{
+        int landIndex = 0;
+		filed[indexBug] = 0;
+        if (direction == "left")
+        {
+		    landIndex = indexBug - steps;
+            if (landIndex < 0)
+            {
+                continue;
+            }
+            while (filed[landIndex] == 1)
+			{
+				landIndex -= steps;
+                if (landIndex < 0)
+                {
+                    break;
+                }
+            }
+            if (landIndex < 0)
+            {
+                continue;
+            }
+            filed[landIndex] = 1;
+        }
+        if (direction == "right")
+        {
+            landIndex = indexBug + steps;
+            if (landIndex >= filed.Length)
+            {
+                continue;
+            }
+            while (filed[landIndex] == 1)
+            {
+                landIndex += steps;
+                if (landIndex > filed.Length - 1)
+                {
+                    break;
+                }
+            }
+            if (landIndex >= filed.Length)
+            {
+                continue;
+            }
+            filed[landIndex] = 1;
+        }
+    }
+}
+Console.WriteLine(string.Join(" ",filed));
