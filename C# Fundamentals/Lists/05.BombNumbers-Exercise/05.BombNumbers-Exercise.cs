8eee9df3f0c66e843986584b46305a9434de84e8
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 15:56:14 +0200

    Add files via upload

---
+++ C# Fundamentals/Lists/05.BombNumbers-Exercise/05.BombNumbers-Exercise.cs
@@ -0,0 +1,13 @@
+﻿List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+List<int> bombList = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+int bomb = bombList[0];
+int bombPower = bombList[1];
+while (numbers.Contains(bomb))
+{
+    int index = numbers.IndexOf(bomb);
+    int leftIndex = Math.Max(0, index - bombPower);
+    int rightIndex = Math.Min(numbers.Count - 1, index + bombPower);
+    int range = rightIndex - leftIndex + 1;
+    numbers.RemoveRange(leftIndex, range);
+}
+Console.WriteLine(numbers.Sum());
