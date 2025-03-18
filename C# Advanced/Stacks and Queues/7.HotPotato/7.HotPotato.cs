b3508d3e237399485f394a212aa3f8fabb9316a6
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 18:48:35 +0200

    Add files via upload

---
+++ C# Advanced/Stacks and Queues/7.HotPotato/7.HotPotato.cs
@@ -0,0 +1,18 @@
+﻿Queue<string> kids = new Queue<string>(Console.ReadLine().Split());
+int n = int.Parse(Console.ReadLine());
+int tick = 1;
+while (kids.Count > 1)
+{
+    string kid = kids.Dequeue();
+    if (tick == n)
+    {
+        Console.WriteLine($"Removed {kid}");
+        tick = 0;
+    }
+    else
+    {
+        kids.Enqueue(kid);
+    }
+    tick++;
+}
+Console.WriteLine($"Last is {kids.Dequeue()}");
---
+++ C# Advanced/Stacks and Queues/7.TruckTour/7.TruckTour.cs
@@ -0,0 +1,29 @@
+﻿int n = int.Parse(Console.ReadLine());
+Queue<(int fuel, int distance)> stations = new Queue<(int fuel, int distance)>();
+for (int i = 0; i < n; i++)
+{
+    int[] station = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    stations.Enqueue((station[0], station[1]));
+}
+for (int i = 0;i < n; i++)
+{
+    int gas = 0;
+    bool found = true;
+    foreach ((int fuel, int distance) station in stations)
+    {
+        gas += station.fuel;
+        if (gas < station.distance)
+        {
+            found = false;
+            break;
+        }
+        gas -= station.distance;
+    }
+    if (found)
+    {
+        Console.WriteLine(i);
+        return;
+    }
+    (int fuel, int distance) current = stations.Dequeue();
+    stations.Enqueue(current);
+}
