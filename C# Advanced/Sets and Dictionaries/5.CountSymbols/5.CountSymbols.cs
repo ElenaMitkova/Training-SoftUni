3a3df6862ba094fb7da35f109069ecb69216bee9
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 19:04:43 +0200

    Add files via upload

---
+++ C# Advanced/Sets and Dictionaries/5.CountSymbols/5.CountSymbols.cs
@@ -0,0 +1,15 @@
+﻿SortedDictionary<char, int> charactersCount = new SortedDictionary<char, int>();
+string text = Console.ReadLine();
+for (int i = 0; i < text.Length; i++)
+{
+    char element = text[i];
+    if (!charactersCount.ContainsKey(element))
+    {
+        charactersCount[element] = 0;
+    }
+    charactersCount[element]++;
+}
+foreach ((char character, int count) in charactersCount)
+{
+    Console.WriteLine($"{character}: {count} time/s");
+}
---
+++ C# Advanced/Sets and Dictionaries/6.Wardrobe/6.Wardrobe.cs
@@ -0,0 +1,36 @@
+﻿Dictionary<string, Dictionary<string, int>> clothes = new Dictionary<string, Dictionary<string, int>>();
+int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    string[] arguments = Console.ReadLine().Split(" -> ");
+    string colour = arguments[0];
+    string[] items = arguments[1].Split(",");
+    if (!clothes.ContainsKey(colour))
+    {
+        clothes[colour] = new Dictionary<string, int>();
+    }
+    for (int j = 0; j < items.Length; j++)
+    {
+        if (!clothes[colour].ContainsKey(items[j]))
+        {
+            clothes[colour][items[j]] = 0;
+        }
+        clothes[colour][items[j]]++;
+    }
+}
+string[] item = Console.ReadLine().Split();
+string color = item[0];
+string thing = item[1];
+foreach ((string colour, Dictionary<string, int> clothing) in clothes)
+{
+    Console.WriteLine($"{colour} clothes:");
+    foreach ((string name, int count) in clothing)
+    {
+        Console.Write($"* {name} - {count}");
+        if (colour == color && name == thing)
+        {
+            Console.Write(" (found!)");
+        }
+        Console.WriteLine();
+    }
+}
