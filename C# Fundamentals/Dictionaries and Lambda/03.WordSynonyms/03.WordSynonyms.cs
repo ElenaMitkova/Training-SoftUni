f735b5649e641bd9ee5a4707f0ed3159962e621e
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 16:16:21 +0200

    Add files via upload

---
+++ C# Fundamentals/Dictionaries and Lambda/03.WordSynonyms/03.WordSynonyms.cs
@@ -0,0 +1,19 @@
+﻿Dictionary<string, List<string>> wordsWithSynonyms = new Dictionary<string, List<string>>();
+int counter = int.Parse(Console.ReadLine());
+for (int i = 0; i < counter; i++)
+{
+    string key = Console.ReadLine();
+    string value = Console.ReadLine();
+    if (wordsWithSynonyms.ContainsKey(key))
+    {
+        wordsWithSynonyms[key].Add(value);
+    }
+    else
+    {
+        wordsWithSynonyms.Add(key, new List<string>() { value });
+    }
+}
+foreach (var word in wordsWithSynonyms)
+{
+    Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}");
+}
