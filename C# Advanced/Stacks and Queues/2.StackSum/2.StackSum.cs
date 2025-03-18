cafdc8d30e31b46a919fa1c1d0d129149c544fbc
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 18:43:52 +0200

    Add files via upload

---
+++ C# Advanced/Stacks and Queues/2.StackSum/2.StackSum.cs
@@ -0,0 +1,22 @@
+﻿Stack<int> numbers = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+string command;
+while ((command = Console.ReadLine().ToLower()) != "end")
+{
+    string[] arguments = command.Split();
+    if (arguments[0] == "add")
+    {
+        numbers.Push(int.Parse(arguments[1]));
+        numbers.Push(int.Parse(arguments[2]));
+    }
+    else
+    {
+        if (numbers.Count >= int.Parse(arguments[1]))
+        {
+            for (int i = 0; i < int.Parse(arguments[1]); i++)
+            {
+                numbers.Pop();
+            }
+        }
+    }
+}
+Console.WriteLine($"Sum: {numbers.Sum()}");
