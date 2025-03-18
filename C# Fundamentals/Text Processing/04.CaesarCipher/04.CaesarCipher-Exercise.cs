91e6e72b614980559150a8656f6c3226ff251ae9
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 16:25:40 +0200

    Add files via upload

---
+++ C# Fundamentals/Text Processing/04.CaesarCipher/04.CaesarCipher-Exercise.cs
@@ -0,0 +1,7 @@
+﻿string text = Console.ReadLine();
+string enc = string.Empty;
+for (int i = 0; i < text.Length; i++)
+{
+    enc += (char)(text[i] + 3);
+}
+Console.WriteLine(enc);
---
+++ C# Fundamentals/Text Processing/04.TextFilter/04.TextFilter.cs
@@ -0,0 +1,10 @@
+﻿string[] filter = Console.ReadLine().Split(", ");
+string text = Console.ReadLine();
+for (int i = 0; i < filter.Length; i++)
+{
+	while (text.Contains(filter[i]))
+	{
+		text = text.Replace(filter[i], new string('*', filter[i].Length));
+	}
+}
+Console.WriteLine(text);
