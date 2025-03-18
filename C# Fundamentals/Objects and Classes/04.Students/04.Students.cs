297db48073974742b755d815fcd35ad54eefc5d0
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 16:08:25 +0200

    Add files via upload

---
+++ C# Fundamentals/Objects and Classes/04.Students/04.Students.cs
@@ -0,0 +1,27 @@
+﻿List<Student> listOfStudents = new List<Student>();
+string command;
+while ((command = Console.ReadLine()) != "end")
+{
+    string[] person = command.Split();
+    Student student = new Student();
+    student.FirstName = person[0];
+    student.SecondName = person[1];
+    student.Age = int.Parse(person[2]);
+    student.HomeTown = person[3];
+    listOfStudents.Add(student);
+}
+string homeTown = Console.ReadLine();
+foreach (Student student in listOfStudents)
+{
+    if (student.HomeTown == homeTown)
+    {
+        Console.WriteLine($"{student.FirstName} {student.SecondName} is {student.Age} years old.");
+    }
+}
+class Student
+{
+    public string FirstName { get; set; }
+    public string SecondName { get; set; }
+    public int Age { get; set; }
+    public string HomeTown { get; set; }
+}
