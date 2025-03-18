21b6c1f06b97e0cdb93660cf3856517fc393a032
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 18:33:50 +0200

    Add files via upload

---
+++ C# Advanced/Stacks and Queues/1.BasicStackOperations/1.BasicStackOperations.cs
@@ -0,0 +1,35 @@
+﻿int[] integers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int n = integers[0];
+int s = integers[1];
+int x = integers[2];
+int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+Stack<int> nums = new Stack<int>();
+for (int i = 0; i < n; i++)
+{
+    nums.Push(numbers[i]);
+}
+for (int i = 0; i < s; i++)
+{
+    nums.Pop();
+}
+if (nums.Count == 0)
+{
+    Console.WriteLine(0);
+}
+else
+{
+    int minNumber = int.MaxValue;
+    foreach (int number in nums)
+    {
+        if (number == x)
+        {
+            Console.WriteLine("true");
+            return;
+        }
+        if (number < minNumber)
+        {
+            minNumber = number;
+        }
+    }
+    Console.WriteLine(minNumber);
+}
---
+++ C# Advanced/Stacks and Queues/1.ReverseAString/1.ReverseAString.cs
@@ -0,0 +1,7 @@
+﻿string input = Console.ReadLine();
+Stack<char> reversed = new Stack<char>();
+for (int i = 0; i < input.Length; i++)
+{
+    reversed.Push(input[i]);
+}
+Console.WriteLine(string.Join("", reversed));
