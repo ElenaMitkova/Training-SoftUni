d3fd259454d29d9883037a86668f3a46bad3dbb4
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 18:26:46 +0200

    Add files via upload

---
+++ C# Fundamentals/Regular Expressions/01.Furniture/01.Furniture.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+string broughts = "";
+decimal total = 0.0m;
+string pattern = @">>(?<Name>[A-Z]+[a-z]*)<<(?<Price>\d+[.\d]+)!(?<Quantity>\d*)";
+string input;
+while ((input = Console.ReadLine()) != "Purchase")
+{
+        broughts += input + " ";
+}
+MatchCollection matches = Regex.Matches(broughts, pattern);
+Console.WriteLine($"Bought furniture:");
+foreach (Match match in matches)
+{
+    Console.WriteLine(match.Groups["Name"]);
+    total += decimal.Parse(match.Groups["Price"].ToString()) * decimal.Parse(match.Groups["Quantity"].ToString());
+}
+Console.WriteLine($"Total money spend: {total:F2}");
---
+++ C# Fundamentals/Regular Expressions/01.MatchFullName/01.MatchFullName.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+string text = Console.ReadLine();
+string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
+MatchCollection matches = Regex.Matches(text, pattern);
+foreach (Match name in matches)
+{
+    Console.Write(name + " ");
+}
+Console.WriteLine();
