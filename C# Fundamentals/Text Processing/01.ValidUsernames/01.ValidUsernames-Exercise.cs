fd1381d402444b73e884a9f21b5cf8936a6ae3e3
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 16:21:37 +0200

    Add files via upload

---
+++ C# Fundamentals/Text Processing/01.ValidUsernames/01.ValidUsernames-Exercise.cs
@@ -0,0 +1,9 @@
+﻿string[] usernames = Console.ReadLine().Split(", ");
+for (int i = 0; i < usernames.Length; i++)
+{
+    bool all = usernames[i].All(user => char.IsLetterOrDigit(user) || user == '-' || user == '_');
+	if (all && usernames[i].Length > 3 && usernames[i].Length < 16)
+	{
+		Console.WriteLine(usernames[i]);
+	}
+}
