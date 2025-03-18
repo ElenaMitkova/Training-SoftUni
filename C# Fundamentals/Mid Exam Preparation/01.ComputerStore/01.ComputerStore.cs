0e175df27ab3c70724f7132ffe877bc9bee6214f
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 15:30:34 +0200

    Add files via upload

---
+++ C# Fundamentals/Mid Exam Preparation/01.ComputerStore/01.ComputerStore.cs
@@ -0,0 +1,29 @@
+﻿string commands;
+double sum = 0.0;
+double total = 0.0;
+while ((commands = Console.ReadLine()) is not "special" and not "regular")
+{
+    if (double.Parse(commands) <= 0)
+    {
+        Console.WriteLine("Invalid price!");
+        continue;
+    }
+    sum += double.Parse(commands);
+    total += double.Parse(commands);
+}
+double taxes = total * 0.2;
+total += taxes;
+if (commands == "special")
+{
+    total -= total * 0.1;
+}
+if (total <= 0.0)
+{
+    Console.WriteLine("Invalid order!");
+    return;
+}
+Console.WriteLine("Congratulations you've just bought a new computer!");
+Console.WriteLine($"Price without taxes: {sum:F2}$");
+Console.WriteLine($"Taxes: {taxes:F2}$");
+Console.WriteLine("-----------");
+Console.WriteLine($"Total price: {total:F2}$");
---
+++ C# Fundamentals/Mid Exam Preparation/02.ShoppingList/02.ShoppingList.cs
@@ -0,0 +1,42 @@
+﻿List<string> shoppingList = Console.ReadLine().Split('!').ToList();
+string command;
+while ((command = Console.ReadLine()) != "Go Shopping!")
+{
+    string[] commands = command.Split();
+    switch (commands[0])
+    {
+        case "Urgent":
+            string item = commands[1];
+            if (!shoppingList.Contains(item))
+            {
+                shoppingList.Insert(0, item);
+            }
+            break;
+        case "Unnecessary":
+            string unItem = commands[1];
+            if (shoppingList.Contains(unItem))
+            {
+                shoppingList.Remove(unItem);
+            }
+            break;
+        case "Correct":
+            string oldName = commands[1];
+            string newName = commands[2];
+            if (shoppingList.Contains(oldName))
+            {
+                int index = shoppingList.IndexOf(oldName);
+                shoppingList.RemoveAt(index);
+                shoppingList.Insert(index, newName);
+            }
+            break;
+        case "Rearrange":
+            string reItem = commands[1];
+            if (shoppingList.Contains(reItem))
+            {
+                shoppingList.Remove(reItem);
+                shoppingList.Add(reItem);
+            }
+            break;
+    }
+}
+Console.WriteLine(string.Join(", ", shoppingList));
