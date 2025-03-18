99657267a7ebd3eec77104a9228af59d075b2fdd
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 16:24:07 +0200

    Add files via upload

---
+++ C# Fundamentals/Text Processing/03.ExtractFile/03.ExtractFile-Exercise.cs
@@ -0,0 +1,8 @@
+﻿string text = Console.ReadLine();
+int indexDot = text.LastIndexOf('.');
+string extension = text.Substring(indexDot + 1);
+int index = text.LastIndexOf('\\');
+string file = text.Substring(index + 1);
+string fileName = file.Substring(0, file.Length - extension.Length - 1);
+Console.WriteLine($"File name: {fileName}");
+Console.WriteLine($"File extension: {extension}");
---
+++ C# Fundamentals/Text Processing/03.Substring/03.Substring.cs
@@ -0,0 +1,7 @@
+﻿string searchedString = Console.ReadLine();
+string text = Console.ReadLine();
+while (text.Contains(searchedString))
+{
+    text = text.Remove(text.IndexOf(searchedString), searchedString.Length);
+}
+Console.WriteLine(text);
