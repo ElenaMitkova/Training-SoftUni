cbd121076934643861691ad488a3920027faa1b8
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 16:27:00 +0200

    Add files via upload

---
+++ C# Fundamentals/Text Processing/05.Digits,LettersandOther/05.Digits,LettersandOther.cs
@@ -0,0 +1,22 @@
+﻿string text = Console.ReadLine();
+string digits = "";
+string leters = "";
+string others = "";
+for (int i = 0; i < text.Length; i++)
+{
+    if (char.IsDigit(text[i]))
+    {
+        digits += text[i];
+    }
+    else if (char.IsLetter(text[i]))
+    {
+        leters += text[i];
+    }
+    else
+    {
+        others += text[i];
+    }
+}
+Console.WriteLine(digits);
+Console.WriteLine(leters);
+Console.WriteLine(others);
---
+++ C# Fundamentals/Text Processing/05.HTML/05.HTML-MoreExercise.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+string title = Console.ReadLine();
+StringBuilder builder = new StringBuilder();
+builder.AppendLine("<h1>");
+builder.AppendLine($"   {title}");
+builder.AppendLine("</h1>");
+string titleContent = Console.ReadLine();
+builder.AppendLine("<article>");
+builder.AppendLine($"   {titleContent}");
+builder.AppendLine("</article>");
+string comments;
+while ((comments = Console.ReadLine()) != "end of comments")
+{
+    builder.AppendLine("<div>");
+    builder.AppendLine($"   {comments}");
+    builder.AppendLine("</div>");
+}
+Console.WriteLine(builder.ToString());
