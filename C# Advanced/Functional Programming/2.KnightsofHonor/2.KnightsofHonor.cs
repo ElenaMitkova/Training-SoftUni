9d3466914c8f9459ebec93229541181af9ab90a8
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 19:42:34 +0200

    Add files via upload

---
+++ C# Advanced/Functional Programming/2.KnightsofHonor/2.KnightsofHonor.cs
@@ -0,0 +1,6 @@
+﻿string[] names = Console.ReadLine().Split();
+Action<string> printingNames = x => Console.WriteLine($"Sir {x}");
+foreach (string name in names)
+{
+    printingNames(name);
+}
---
+++ C# Advanced/Functional Programming/2.SumNumbers/2.SumNumbers.cs
@@ -0,0 +1,4 @@
+﻿Func<string, int> selector = x => int.Parse(x);
+int[] numbers = Console.ReadLine().Split(", ").Select(selector).ToArray();
+Console.WriteLine(numbers.Length);
+Console.WriteLine(numbers.Sum());
