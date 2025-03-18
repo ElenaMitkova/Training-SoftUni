0647bb6506b216ca33161a6c024200725850337e
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 18:46:53 +0200

    Add files via upload

---
+++ C# Advanced/Stacks and Queues/5.FashionBoutique/5.FashionBoutique.cs
@@ -0,0 +1,20 @@
+﻿Stack<int> clothes = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+int rackCappasity = int.Parse(Console.ReadLine());
+int remaining = 0;
+int racks = 1;
+while (clothes.Count > 0)
+{
+	int cloth = clothes.Peek();
+	if (remaining + cloth <= rackCappasity)
+	{
+		remaining += cloth;
+		clothes.Pop();
+	}
+	else
+	{
+		remaining = cloth;
+		racks++;
+		clothes.Pop();
+	}
+}
+Console.WriteLine(racks);
---
+++ C# Advanced/Stacks and Queues/5.PrintEvenNumbers/5.PrintEvenNumbers.cs
@@ -0,0 +1,12 @@
+﻿Queue<int> numbers = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+int count = numbers.Count();
+while (count > 0)
+{
+    int number = numbers.Dequeue();
+    if (number % 2 == 0)
+    {
+        numbers.Enqueue(number);
+    }
+    count--;
+}
+Console.WriteLine(string.Join(", ", numbers));
