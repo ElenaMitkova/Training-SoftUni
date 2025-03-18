c217febd74b16a4aa7cd64e4019e15def226385f
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 15:57:29 +0200

    Add files via upload

---
+++ C# Fundamentals/Lists/06.CardsGame-Exercise/06.CardsGame-Exercise.cs
@@ -0,0 +1,32 @@
+﻿List<int> firstCards = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+List<int> secondCards = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+while (firstCards.Count > 0 && secondCards.Count > 0)
+{
+    int firstCard = firstCards[0];
+    int secondCard = secondCards[0];
+	if (firstCard > secondCard)
+	{
+		firstCards.RemoveAt(0);
+		secondCards.RemoveAt(0);
+		firstCards.Add(secondCard);
+        firstCards.Add(firstCard);
+    }
+    else if (secondCard > firstCard) 
+    {
+        firstCards.RemoveAt(0);
+        secondCards.RemoveAt(0);
+        secondCards.Add(firstCard);
+        secondCards.Add(secondCard);
+    }
+    else
+    {
+        firstCards.RemoveAt(0);
+        secondCards.RemoveAt(0);
+    }
+}
+if (firstCards.Count == 0)
+{
+    Console.WriteLine($"Second player wins! Sum: {secondCards.Sum()}");
+    return;
+}
+Console.WriteLine($"First player wins! Sum: {firstCards.Sum()}");
---
+++ C# Fundamentals/Lists/06.ListManipulationBasics/06.ListManipulationBasics.cs
@@ -0,0 +1,31 @@
+﻿List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+string input;
+while ((input = Console.ReadLine()) != "end")
+{
+    string[] commands = input.Split();
+    string command = commands[0];
+    switch (command)
+    {
+        case "Add":
+            int addNumber = int.Parse(commands[1]);
+            numbers.Add(addNumber);
+            break;
+        case "Remove":
+            int removeNumber = int.Parse(commands[1]);
+            if (numbers.Contains(removeNumber))
+            {
+                numbers.Remove(removeNumber);
+            }
+            break;
+        case "RemoveAt":
+            int removeIndex = int.Parse(commands[1]);
+            numbers.RemoveAt(removeIndex);
+            break;
+        case "Insert":
+            int insertNumber = int.Parse(commands[1]);
+            int insertIndex = int.Parse(commands[2]);
+            numbers.Insert(insertIndex, insertNumber);
+            break;
+    }
+}
+Console.WriteLine(string.Join(' ', numbers));
