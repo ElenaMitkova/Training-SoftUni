e76db4861d0196df5f646678fff1a50850e7fc07
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 16:11:50 +0200

    Add files via upload

---
+++ C# Fundamentals/Objects and Classes/07.OrderbyAge/07.OrderbyAge-Exercise.cs
@@ -0,0 +1,33 @@
+﻿List<Person> people = new List<Person>();
+string input;
+while ((input = Console.ReadLine()) != "End")
+{
+    string[] arguments = input.Split();
+    Person newPerson = people.FirstOrDefault(person => person.Id == int.Parse(arguments[1]));
+    if (newPerson != null)
+    {
+        newPerson.Age = int.Parse(arguments[2]);
+        newPerson.Name = arguments[0];
+    }
+    else
+    {
+        people.Add(new Person(arguments[0], int.Parse(arguments[1]), int.Parse(arguments[2])));
+    }
+}
+people = people.OrderBy(person => person.Age).ToList();
+foreach (var person in people)
+{
+    Console.WriteLine($"{person.Name} with ID: {person.Id} is {person.Age} years old.");
+}
+class Person
+{
+    public Person(string name, int id, int age)
+    {
+        Name = name;
+        Id = id;
+        Age = age;
+    }
+    public string Name { get; set; }
+    public int Id { get; set; }
+    public int Age { get; set; }
+}
---
+++ C# Fundamentals/Objects and Classes/07.VehicleCatalogue/07.VehicleCatalogue.cs
@@ -0,0 +1,56 @@
+﻿Catalogue catalogue = new Catalogue();
+string command;
+while ((command = Console.ReadLine()) != "end")
+{
+    string[] item = command.Split('/');
+    if (item[0] == "Car")
+    {
+        Car car = new Car { Brand = item[1], Model = item[2], HorsePower = double.Parse(item[3]) };
+        catalogue.Cars.Add(car);
+    }
+    else
+    {
+        Truck truck = new Truck { Brand = item[1], Model = item[2], Weight = double.Parse(item[3]) };
+        catalogue.Trucks.Add(truck);
+    }
+}
+if (catalogue.Cars.Count > 0)
+{
+    List <Car> newCars = catalogue.Cars.OrderBy(x => x.Brand).ToList();
+    Console.WriteLine("Cars:");
+    foreach (var item in newCars)
+    {
+        Console.WriteLine($"{item.Brand}: {item.Model} - {item.HorsePower}hp");
+    }
+}
+if (catalogue.Trucks.Count > 0)
+{
+    List<Truck> newTrucks = catalogue.Trucks.OrderBy(x => x.Brand).ToList();
+    Console.WriteLine("Trucks:");
+    foreach (var item in newTrucks)
+    {
+        Console.WriteLine($"{item.Brand}: {item.Model} - {item.Weight}kg");
+    }
+}
+class Truck
+{
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public double Weight { get; set; }
+}
+class Car
+{
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public double HorsePower { get; set; }
+}
+class Catalogue
+{
+    public List<Truck> Trucks { get; set; }
+    public List<Car> Cars { get; set; }
+    public Catalogue()
+    {
+        Trucks = new List<Truck>();
+        Cars = new List<Car>();
+    }
+}
