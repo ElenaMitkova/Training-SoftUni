d1ca4dcdd50ea3f3c9bd6a7f882c2cda5434f107
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 15:51:05 +0200

    Add files via upload

---
+++ C# Fundamentals/Lists/02.ChangeList-Exercise/02.ChangeList-Exercise.cs
@@ -0,0 +1,25 @@
+﻿List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+string commands;
+while ((commands = Console.ReadLine()) != "end")
+{
+    string[] command = commands.Split();
+	if (command[0] == "Delete")
+	{
+		int searchedNumber = int.Parse(command[1]);
+		for (int i = 0; i < numbers.Count; i++)
+		{
+			if (numbers[i] == searchedNumber)
+			{
+				numbers.RemoveAt(i);
+				i--;
+			}
+		}
+	}
+	else if (command[0] == "Insert")
+	{
+		int insertedNumber = int.Parse(command[1]);
+		int position = int.Parse(command[2]);
+		numbers.Insert(position, insertedNumber);
+	}
+}
+Console.WriteLine(string.Join(" ", numbers));
