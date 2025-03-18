ac83ef7664c0de8aa8252df06071c614fb979c68
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 18:59:51 +0200

    Add files via upload

---
+++ C# Advanced/Sets and Dictionaries/02.AverageStudentGrades/2.AverageStudentGrades.cs
@@ -0,0 +1,22 @@
+﻿int n = int.Parse(Console.ReadLine());
+Dictionary<string, List<decimal>> grades = new Dictionary<string, List<decimal>>();
+for (int i = 0; i < n; i++)
+{
+    string[] arguments = Console.ReadLine().Split();
+    string student = arguments[0];
+    decimal grade = decimal.Parse(arguments[1]);
+    if (!grades.ContainsKey(student))
+    {
+        grades.Add(student, new List<decimal>());
+    }
+    grades[student].Add(grade);
+}
+foreach ((string student, List<decimal> grade) in grades)
+{
+    Console.Write($"{student} -> ");
+    foreach (decimal grading in grade)
+    {
+        Console.Write($"{grading:f2} ");
+    }
+    Console.WriteLine($"(avg: {grade.Average():f2})");
+}
---
+++ C# Advanced/Sets and Dictionaries/1.CountSameValuesinArray/1.CountSameValuesinArray.cs
@@ -0,0 +1,14 @@
+﻿double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+Dictionary<double, int> numbersCount = new Dictionary<double, int>();
+for (int i = 0; i < numbers.Length; i++)
+{
+	if (!numbersCount.ContainsKey(numbers[i]))
+	{
+		numbersCount[numbers[i]] = 0;
+	}
+	numbersCount[numbers[i]]++;
+}
+foreach (KeyValuePair<double, int> number in numbersCount)
+{
+    Console.WriteLine($"{number.Key} - {number.Value} times");
+}
