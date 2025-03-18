f2882d0648b6efd72d73a1ab570f515ded5ec531
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 18:45:45 +0200

    Add files via upload

---
+++ C# Advanced/Stacks and Queues/04.FastFood/4.FastFood.cs
@@ -0,0 +1,18 @@
+﻿int food = int.Parse(Console.ReadLine());
+Queue<int> orders = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+Console.WriteLine(orders.Max());
+while (orders.Count > 0)
+{
+    int order = orders.Peek();
+    if (order <= food)
+    {
+        orders.Dequeue();
+        food -= order;
+    }
+    else
+    {
+        Console.WriteLine($"Orders left: {string.Join(' ', orders)}");
+        return;
+    }
+}
+Console.WriteLine("Orders complete");
---
+++ C# Advanced/Stacks and Queues/4.MatchingBrackets/4.MatchingBrackets.cs
@@ -0,0 +1,14 @@
+﻿string input = Console.ReadLine();
+Stack<int> brackets = new Stack<int>();
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i] == '(')
+    {
+        brackets.Push(i);
+    }
+    else if (input[i]== ')')
+    {
+        int opened = brackets.Pop();
+        Console.WriteLine(input.Substring(opened, i - opened + 1));
+    }
+}
