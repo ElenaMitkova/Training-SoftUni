1e36ee0cd8ab6159ae3dbe31da1d22f4a6a71633
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 19:44:57 +0200

    Add files via upload

---
+++ C# Advanced/Functional Programming/5.AppliedArithmetics/5.AppliedArithmetics.cs
@@ -0,0 +1,20 @@
+﻿Dictionary<string, Action<int[]>> transformByCommand = new Dictionary<string, Action<int[]>>()
+{
+    ["add"] = array => TransformingArray(array, x => x + 1),
+    ["multiply"] = array => TransformingArray(array, x => x * 2),
+    ["subtract"] = array => TransformingArray(array, x => x - 1),
+    ["print"] = array => Console.WriteLine(string.Join(' ', array)),
+};
+int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+string command;
+while ((command = Console.ReadLine()) != "end")
+{
+    transformByCommand[command](numbers);
+}
+void TransformingArray(int[] numbers, Func<int, int> transform)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = transform(numbers[i]);
+    }
+}
---
+++ C# Advanced/Functional Programming/5.FilterByAge/5.FilterByAge.cs
@@ -0,0 +1,44 @@
+﻿List<Person> people = new List<Person>(); 
+int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    string[] person = Console.ReadLine().Split(", ");
+    people.Add(new Person(person[0], int.Parse(person[1])));
+}
+string condition = Console.ReadLine();
+int age = int.Parse(Console.ReadLine());
+Func<Person, bool> predicate = null;
+if (condition == "younger")
+{
+    predicate = p => p.Age < age;
+}
+else if (condition == "older")
+{
+    predicate = p => p.Age >= age;
+}
+people = people.Where(predicate).ToList();
+string format = Console.ReadLine();
+Action<Person> action = null;
+if (format == "name")
+{
+    action = p => Console.WriteLine(p.Name);
+}
+else if (format == "age")
+{
+    action = p => Console.WriteLine(p.Age);
+}
+else if (format == "name age")
+{
+    action = p => Console.WriteLine($"{p.Name} - {p.Age}");
+}
+people.ForEach(action);
+public class Person
+{
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
