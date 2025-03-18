49b210e5c9c563f293e5b898adebb7e16dbe748e
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 18:10:48 +0200

    Add files via upload

---
+++ C# Fundamentals/Text Processing/06.ReplaceRepeatingChars/06.ReplaceRepeatingChars-Exercise.cs
@@ -0,0 +1,10 @@
+﻿string text = Console.ReadLine();
+string result = text[0].ToString();
+for (int i = 1; i < text.Length; i++)
+{
+    if (text[i] != text[i - 1])
+    {
+        result += text[i];
+    }
+}
+Console.WriteLine(result);
