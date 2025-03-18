49443af10642f4c1001ac0c55391e8ed93d8fff3
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 15:52:38 +0200

    Add files via upload

---
+++ C# Fundamentals/Lists/03.HouseParty-Exercise/03.HouseParty-Exercise.cs
@@ -0,0 +1,24 @@
+﻿int counter = int.Parse(Console.ReadLine());
+List<string> names = new List<string>();
+while (counter > 0)
+{
+    string[] command = Console.ReadLine().Split();
+    string name = command[0];
+    switch (command[2])
+    {
+        case "going!" when !names.Contains(name):
+            names.Add(name);
+            break;
+        case "going!" when names.Contains(name):
+            Console.WriteLine($"{name} is already in the list!");
+            break;
+        case "not" when names.Contains(name):
+            names.Remove(name);
+            break;
+        case "not" when !names.Contains(name):
+            Console.WriteLine($"{name} is not in the list!");
+            break;
+    }
+    counter--;
+}
+Console.WriteLine(string.Join("\n", names));
---
+++ C# Fundamentals/Lists/03.MergingLists/03.MergingLists.cs
@@ -0,0 +1,27 @@
+﻿List<int> firstListOfNumbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+List<int> secondListOfNumbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+List<int> numbers = new List<int>();
+if (firstListOfNumbers.Count > secondListOfNumbers.Count)
+{
+    for (int i = 0; i < secondListOfNumbers.Count; i++)
+    {
+        numbers.Add(firstListOfNumbers[i]);
+        numbers.Add(secondListOfNumbers[i]);
+    }
+    for (int i = secondListOfNumbers.Count; i < firstListOfNumbers.Count; i++)
+    {
+        numbers.Add(firstListOfNumbers[i]);
+    }
+    Console.WriteLine(string.Join(' ', numbers));
+    return;
+}
+for (int i = 0; i < firstListOfNumbers.Count; i++)
+{
+    numbers.Add(firstListOfNumbers[i]);
+    numbers.Add(secondListOfNumbers[i]);
+}
+for (int i = firstListOfNumbers.Count; i < secondListOfNumbers.Count; i++)
+{
+    numbers.Add(secondListOfNumbers[i]);
+}
+Console.WriteLine(string.Join(' ', numbers));
