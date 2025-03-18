c7d127bef2fd03bc7fea53cbe8f8609ab9ee93d8
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 16:15:04 +0200

    Add files via upload

---
+++ C# Fundamentals/Dictionaries and Lambda/02.AMinerTask/02.AMinerTask-Exercise.cs
@@ -0,0 +1,16 @@
+﻿Dictionary<string, int> resources = new Dictionary<string, int>();
+string input;
+while ((input = Console.ReadLine()) != "stop")
+{
+    string resource = input;
+    int quantity = int.Parse(Console.ReadLine());
+    if (!resources.ContainsKey(resource))
+    {
+        resources.Add(resource, 0);
+    }
+    resources[resource] += quantity;
+}
+foreach (var pair in resources)
+{
+    Console.WriteLine($"{pair.Key} -> {pair.Value}");
+}
---
+++ C# Fundamentals/Dictionaries and Lambda/02.Judge/02.Judge-MoreExercise.cs
@@ -0,0 +1,54 @@
+﻿Dictionary<string, Dictionary<string, int>> submissions = new Dictionary<string, Dictionary<string, int>>();
+Dictionary<string, Dictionary<string, int>> users = new Dictionary<string, Dictionary<string, int>>();
+string line;
+while ((line = Console.ReadLine().Trim()) != "no more time")
+{
+    var parts = line.Split(" -> ");
+    string username = parts[0];
+    string contest = parts[1];
+    int points = int.Parse(parts[2]);
+    if (!submissions.ContainsKey(contest))
+    {
+        submissions[contest] = new Dictionary<string, int>();
+    }
+    if (!submissions[contest].ContainsKey(username))
+    {
+        submissions[contest][username] = points;
+    }
+    else
+    {
+        submissions[contest][username] = Math.Max(submissions[contest][username], points);
+    }
+    if (!users.ContainsKey(username))
+    {
+        users[username] = new Dictionary<string, int>();
+    }
+    if (!users[username].ContainsKey(contest))
+    {
+        users[username][contest] = points;
+    }
+    else
+    {
+        users[username][contest] = Math.Max(users[username][contest], points);
+    }
+}
+foreach (var contest in submissions)
+{
+    Console.WriteLine($"{contest.Key}: {contest.Value.Count} participants");
+    var sortedParticipants = contest.Value.OrderByDescending(x => x.Value).ThenBy(x => x.Key).ToList();
+    int rank = 1;
+    foreach (var participant in sortedParticipants)
+    {
+        Console.WriteLine($"{rank}. {participant.Key} <::> {participant.Value}");
+        rank++;
+    }
+}
+Console.WriteLine("Individual standings:");
+var sortedUsers = users.OrderByDescending(x => x.Value.Values.Sum()).ThenBy(x => x.Key).ToList();
+int userRank = 1;
+foreach (var user in sortedUsers)
+{
+    int totalPoints = user.Value.Values.Sum();
+    Console.WriteLine($"{userRank}. {user.Key} -> {totalPoints}");
+    userRank++;
+}
