d32de62ed4fff2e1e1a7a64628a4414fae1a6ecc
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 18:47:50 +0200

    Add files via upload

---
+++ C# Advanced/Stacks and Queues/6.SongsQueue/6.SongsQueue.cs
@@ -0,0 +1,30 @@
+﻿Queue<string> songs = new Queue<string>(Console.ReadLine().Split(", "));
+while (songs.Count > 0)
+{
+    string[] command = Console.ReadLine().Split();
+	switch (command[0])
+	{
+		case "Play":
+			songs.Dequeue();
+			break;
+		case "Add":
+			string newsong = "";
+			for (int i = 1; i < command.Length; i++)
+			{
+				newsong += command[i];
+			}
+			if (songs.Contains(newsong))
+			{
+				Console.WriteLine($"{newsong} is already contained!");
+			}
+			else
+			{
+				songs.Enqueue(newsong);
+			}
+			break;
+		case "Show":
+            Console.WriteLine(string.Join(", ", songs));
+			break;
+    }
+}
+Console.WriteLine("No more songs!");
---
+++ C# Advanced/Stacks and Queues/6.Supermarket/6.Supermarket.cs
@@ -0,0 +1,20 @@
+﻿Queue<string> customers = new Queue<string>();
+int count = 0;
+string command;
+while ((command = Console.ReadLine()) != "End")
+{
+    if (command == "Paid")
+    {
+        while (customers.Count > 0)
+        {
+            Console.WriteLine(customers.Dequeue());
+        }
+        count = 0;
+    }
+    else
+    {
+        customers.Enqueue(command);
+        count++;
+    }
+}
+Console.WriteLine($"{count} people remaining.");
