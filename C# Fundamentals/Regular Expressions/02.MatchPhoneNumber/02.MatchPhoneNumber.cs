53a03a4821ae88d8666a56d69d3bac50672a4a8e
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 18:27:49 +0200

    Add files via upload

---
+++ C# Fundamentals/Regular Expressions/02.MatchPhoneNumber/02.MatchPhoneNumber.cs
@@ -0,0 +1,7 @@
+﻿using System.Text.RegularExpressions;
+
+string pattern = @"\+359[- ]2[ -]\d{3}[ -]\d{4}";
+string numbers = Console.ReadLine();
+MatchCollection matches = Regex.Matches(numbers, pattern);
+var phones = matches.Cast<Match>().Select(phone => phone.Value.Trim()).ToArray();
+Console.WriteLine(string.Join(", ", phones));
---
+++ C# Fundamentals/Regular Expressions/02.Race/02.Race.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+List<string> names = Console.ReadLine().Split(", ").ToList();
+string input;
+Dictionary<string, int> output = new Dictionary<string, int>();
+while ((input = Console.ReadLine()) != "end of race")
+{
+    string name = string.Concat(Regex.Matches(input, @"[A-Za-z]").Select(x => x.Value));
+    int distance = Regex.Matches(input, @"\d").Sum(x => int.Parse(x.Value));
+    if (output.ContainsKey(name) && names.Contains(name))
+    {
+        output[name] += distance;
+    }
+    else if (!output.ContainsKey(name) && names.Contains(name))
+    {
+        output[name] = distance;
+    }
+}
+var namesPlaces = output.OrderByDescending(x => x.Value).Take(3).ToList();
+for (int i = 0; i < namesPlaces.Count; i++)
+{
+    string place = i switch
+    {
+        0 => "1st",
+        1 => "2nd",
+        2 => "3rd"
+    };
+    Console.WriteLine($"{place} place: {namesPlaces[i].Key}");
+}
