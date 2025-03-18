2b66632b5f4e47c06f9653f7aff5e78fd9a7c37b
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-18 18:44:43 +0200

    Add files via upload

---
+++ C# Advanced/Stacks and Queues/3.MaximumandMinimumElement/3.MaximumandMinimumElement.cs
@@ -0,0 +1,34 @@
+﻿int n = int.Parse(Console.ReadLine());
+Stack<int> numbers = new Stack<int>();
+for (int i = 0; i < n; i++)
+{
+    int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+	switch (input[0])
+	{
+		case 1:
+			numbers.Push(input[1]);
+			break;
+		case 2:
+            if (numbers.Count > 0)
+            {
+                numbers.Pop();
+            }
+			break;
+		case 3:
+            if (numbers.Count > 0)
+            {
+                Console.WriteLine(numbers.Max());
+            }
+            break;
+        case 4:
+            if (numbers.Count > 0)
+            {
+                Console.WriteLine(numbers.Min());
+            }
+            break;
+    }
+}
+if (numbers.Count > 0)
+{
+    Console.WriteLine(string.Join(", ", numbers));
+}
---
+++ C# Advanced/Stacks and Queues/3.SimpleCalculator/3.SimpleCalculator.cs
@@ -0,0 +1,17 @@
+﻿string[] input = Console.ReadLine().Split();
+Stack<string> operations = new Stack<string>(input.Reverse());
+int sum = int.Parse(operations.Pop());
+while (operations.Count > 0)
+{
+    string operation = operations.Pop();
+    int number = int.Parse(operations.Pop());
+    if (operation == "+")
+    {
+        sum += number;
+    }
+    else
+    {
+        sum -= number;
+    }
+}
+Console.WriteLine(sum);
